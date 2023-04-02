c6cb194787a41a2fd99c93e68dc408b2715cffd6
Adam Mitha <adam.mitha@gmail.com>
2023-04-02 09:22:41 -0700

    Setup main function for tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+    let client = sparklite_client::RetryingHttpClient::new(hyper::client::HttpConnector::new());
+    println!("{:?}", client.get("http://localhost:8000/").await.unwrap());
 }
