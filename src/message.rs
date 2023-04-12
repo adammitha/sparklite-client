9d9e075506f8d07e92929f7f9e153137d4e4d7e4
Adam Mitha <adam.mitha@gmail.com>
2023-04-12 13:38:35 -0700

    Modify Message to add FilterPredicate

--- src/message.rs
+++ src/message.rs
@@ -1,15 +1,22 @@
+use crate::dataset::FilterPredicate;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
-pub enum Message<'a> {
+pub enum Message<'a, T>
+where
+    T: PartialEq + PartialOrd,
+{
     CreateDataset { id: &'a str, data: &'a [u8] },
     LoadDataset { id: &'a str },
-    Transformation(Transformation),
+    Transformation(Transformation<T>),
 }
 
 #[derive(Serialize, Deserialize)]
-pub enum Transformation {
+pub enum Transformation<T>
+where
+    T: PartialEq + PartialOrd,
+{
     Map,
-    Filter,
+    Filter(FilterPredicate<T>),
     Reduce,
 }
